b4054415ee309d08341c9ddcfb66805c7118e3d1
FilErc <S1107630@studenti.univpm.it>
2025-05-29 18:36:08 +0200

    d

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -2,12 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get web => const FirebaseOptions(
-      apiKey: "AIzaSyD2OMttcjwaq4zVc52U4C4y3hKpr8yUmqY",
-      authDomain: "fantadiv.firebaseapp.com",
-      projectId: "fantadiv",
-      storageBucket: "fantadiv.firebasestorage.app",
-      messagingSenderId: "244659699647",
-      appId: "1:244659699647:web:de45d77f0af231a7ef2b0a",
-      measurementId: "G-7Q3WSKR59B"
+    apiKey: "AIzaSyD2OMttcjwaq4zVc52U4C4y3hKpr8yUmqY",
+    authDomain: "fantadiv.firebaseapp.com",
+    projectId: "fantadiv",
+    storageBucket: "fantadiv.firebasestorage.app",
+    messagingSenderId: "244659699647",
+    appId: "1:244659699647:web:de45d77f0af231a7ef2b0a",
+    measurementId: "G-7Q3WSKR59B",
   );
 }
