6ad59ec27de5c887c1407ccb1d89bea7809fb429
FilErc <S1107630@studenti.univpm.it>
2025-05-30 13:53:06 +0200

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1b76fc9583e2ebdcf942277759a3bae2
 "canvaskit/skwasm.js.symbols": "9fe690d47b904d72c7d020bd303adf16",
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "120a471425dd9ee0926aa6efe63a6626",
+"flutter_bootstrap.js": "e63d0d7662beb2fd2645162edb4e2789",
 "index.html": "90371c58f8004361e880ed340fce349f",
 "/": "90371c58f8004361e880ed340fce349f",
 "main.dart.js": "13440f3809000298d8e5c1c12d7f8a35",
