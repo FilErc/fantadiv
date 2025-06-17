edad7c0acd0d9376daa12969e0c66f55aca99bac
FilErc <S1107630@studenti.univpm.it>
2025-06-17 17:29:08 +0200

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,10 +28,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "22487f9e47273b99893ac79e02b7fb30
 "canvaskit/skwasm.js.symbols": "9fe690d47b904d72c7d020bd303adf16",
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "f39af5a4ccc401e6f1a4253586ee532a",
-"index.html": "f9a16ad0568356dc6f0021c043f46af4",
-"/": "f9a16ad0568356dc6f0021c043f46af4",
-"main.dart.js": "3a19362c8bf5de7fd8ca182983532fbd",
+"flutter_bootstrap.js": "4cedd9c855b1c2a3aefb2ea73ede0e3d",
+"index.html": "8af1b31f1fafa433bd53b8608afcb2c7",
+"/": "8af1b31f1fafa433bd53b8608afcb2c7",
+"main.dart.js": "3153ddfe0271c1d5451739a89a5a29a7",
 "version.json": "e83c8a10cbaa89c321d1dc626fa1562c"};
 // The application shell files that are downloaded before a service worker can
 // start.
